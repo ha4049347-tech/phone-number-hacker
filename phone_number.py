96ab6e8c13b061f13ca991c273ec3dd3b68623cc
Hamid3x3 <ha4049347@gmail.com>
2026-05-04 19:36:45 +0300

    FIxed some typos and that it

---
+++ phone_number.py
@@ -0,0 +1,18 @@
+import phonenumbers
+from phonenumbers import geocoder, carrier
+print("Welcome to Hamid's phone number Hacker! make sure to start with a + !")
+number = input("Enter your phone number and make sure to start with +: ")
+
+try:
+    parsed_number = phonenumbers.parse(number)
+
+    # This gets the short code like 'MA'
+    location_code = phonenumbers.region_code_for_number(parsed_number)
+    print("Location:", location_code)
+
+    # This gets the service provider
+    service_provider = carrier.name_for_number(parsed_number, "en")
+    print("Carrier:", service_provider)
+
+except:
+    print("The string supplied did not seem to be a phone number.")
